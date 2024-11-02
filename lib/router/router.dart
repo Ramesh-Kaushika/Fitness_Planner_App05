6009cdc44052ec56f69aa08a9038db885a05fde0
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-11-02 11:09:12 +0530

    router done

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,21 +1,33 @@
 import 'package:fitness_planner/pages/main_page.dart';
 import 'package:fitness_planner/pages/profile_page.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class RouterClass {
   final router = GoRouter(
+    errorPageBuilder: (context, state) {
+      return const MaterialPage(
+        child: Scaffold(
+          body: Center(
+            child: Text("404 NOT FOUND"),
+          ),
+        ),
+      );
+    },
     routes: [
       //Home Page
-      GoRoute(path: "/", 
-      builder: (context, state){
-        return const MainPage();
-      },
+      GoRoute(
+        path: "/",
+        builder: (context, state) {
+          return const MainPage();
+        },
       ),
-        //Profile
-      GoRoute(path: "/profile", 
-      builder: (context, state){
-        return const ProfilePage();
-      },
+      //Profile
+      GoRoute(
+        path: "/profile",
+        builder: (context, state) {
+          return const ProfilePage();
+        },
       )
     ],
   );
